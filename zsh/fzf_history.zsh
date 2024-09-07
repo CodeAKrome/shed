0bfb568783e1cba9f1eebaed0d0e9f9c9dc4067e
kyle gaspar <kyleg.job@gmail.com>
2024-09-07 11:18:59 -0400

    fzf history search

---
+++ zsh/fzf_history.zsh
@@ -0,0 +1,17 @@
+# fzf history search
+fzf-history-widget() {
+  local selected num
+  setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
+  selected=( $(fc -rl 1 | fzf --height 40% --reverse --bind 'ctrl-r:toggle-sort' +m) )
+  local ret=$?
+  if [ -n "$selected" ]; then
+    num=$selected[1]
+    if [ -n "$num" ]; then
+      zle vi-fetch-history -n $num
+    fi
+  fi
+  zle reset-prompt
+  return $ret
+}
+zle     -N   fzf-history-widget
+bindkey '^R' fzf-history-widget
