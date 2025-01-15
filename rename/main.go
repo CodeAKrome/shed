a80df8fe2f25e0673e8a02abc5bab7dcd4bb2778
kyle gaspar <kyleg.job@gmail.com>
2025-01-15 08:38:00 -0500

    seems ok

--- rename/main.go
+++ rename/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	rename "github.com/CodeAKrome/shed/rename/pkg"
 )
 
 func main() {
-	filename := "my file.with spaces and ?chars 1957.mkv"
-	cleanedFilename := rename.CleanFilename(filename)
-	fmt.Println(cleanedFilename)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		filename := scanner.Text()
+		cleanedFilename := rename.CleanFilename(filename)
+		fmt.Println(filename + "\t" + cleanedFilename)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
 }
--- rename/pkg/rename.go
+++ rename/pkg/rename.go
@@ -6,28 +6,46 @@ import (
 	"strings"
 )
 
-func CleanFilename(filename string) string {
+func parseFilePath(filePath string) (dir string, base string, ext string) {
+	// Get directory and base filename
+	dir, base = filepath.Split(filePath)
+
+	// Remove trailing slash from directory if present
+	dir = strings.TrimSuffix(dir, "/")
+
+	// Get file extension (if any)
+	ext = filepath.Ext(base)
+
+	// Get filename without extension
+	base = strings.TrimSuffix(base, ext)
+
+	return dir, base, ext
+}
+
+// Example filename:
+// Downloads/Trading Places (1983) [2160p] [4K] [BluRay] [5.1] [YTS.MX]/Trading.Places.1983.2160p.4K.BluRay.x265.10bit.AAC5.1-[YTS.MX].mkv
+func CleanFilename(filePath string) (file string) {
 	// Remove leading and trailing spaces
-	filename = strings.TrimSpace(filename)
+	// filename = strings.TrimSpace(file)
+	// Split the filename into parts
+	_, base, ext := parseFilePath(filePath)
 
 	// Replace invalid characters with underscores, except for years
-	re := regexp.MustCompile(`([12]\d{3})`) // Match 4-digit years
-	filename = re.ReplaceAllStringFunc(filename, func(match string) string {
-		return "(" + match + ")"
-	})
+	re := regexp.MustCompile(`.*(19|20)\d{2}`) // Match 4-digit years
+	match := re.FindString(base)
 
-	extension := filepath.Ext(filename)
-	if extension != "" {
-		filename = filename[:len(filename)-len(extension)]
-	}
+	// Surround date with parens
+	re = regexp.MustCompile(`(19|20)\d{2}`)
+	match = re.ReplaceAllString(match, "($0)")
 
 	// Delete invalid characters
-	re2 := regexp.MustCompile(`[^A-Za-z0-9\-\(\)\. ]`)
-	filename = re2.ReplaceAllString(filename, "")
+	re = regexp.MustCompile(`[^A-Za-z0-9\-\(\)\. ]`)
+	filename := re.ReplaceAllString(match, "")
 
 	// Replace periods with spaces
-	re = regexp.MustCompile(`\.`) // Match periods not preceded by a parenthesis
+	re = regexp.MustCompile(`\.`)
 	filename = re.ReplaceAllString(filename, " ")
+	file = filename + ext
 
-	return filename + extension
+	return file
 }
